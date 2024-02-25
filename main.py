099fc99afb5d80995ce15bb10e33d79a4496d908
Hassan Chowdhry <94680098+HassanChowdhry@users.noreply.github.com>
2024-02-24 23:45:42 -0400

    main touches

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import discord
 from discord.ext import commands
 from os import getenv
-from service.leetStats import getLeetStats
+from service.leetStats import getLeetStats, compareUsers
 from dotenv import load_dotenv
 
 # gets env var
 load_dotenv()
 
 # initialsinf bots - switch intents to default and message content to true bfr finish
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), case_insensitive=True)
+bot = commands.Bot(command_prefix="/", intents=discord.Intents.all(), case_insensitive=True)
 
 # event when bot starts
 @bot.event
@@ -17,35 +17,96 @@ async def on_ready():
 
 # bot command - only accepts messages from specific server
 @bot.command()
-async def stats(ctx):
+async def leet(ctx):
     if ctx.channel.name != "testing":
         return
     
-    #reply for stats and sends back as an embedded message
-    quotedText = 'Which *stats* do you want?'
-    embed=discord.Embed(title= quotedText, color=discord.Color.blue())
-    await ctx.send(embed=embed)
-    
-    # for now sends back which applications stats we want
-    msg = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
-    if msg.content.lower() in "leetcode":
+    try:
+        # reply for stats and send back as an embedded message
+        embed = discord.Embed(title="", color=discord.Color.yellow())
         embed.title = "Enter your username"
         await ctx.send(embed=embed)
-        username = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+        
+        # wait for user input
+        username = await bot.wait_for("message", check=lambda m: m.author == ctx.author, timeout=60.0)
         username = username.content
+        
         df = await getLeetStats(username=username)
         
-        # Convert the DataFrame to a string for sending through Discord
-        stats_message = df.to_string(index=False)
-        
-        # Sending the stats as a code block for better formatting
-        await ctx.send(f"```{stats_message}```")
-    
-    else:
-        embed.title = "Not available"
-        await ctx.send(embed=embed)
+        if df is not None:
+            # Convert the DataFrame to a string for sending through Discord
+            stats_message = df.to_string(index=False)
+            
+            # send as code block
+            await ctx.send(f"```{stats_message}```")
+        else:
+            # username not found
+            error_embed = discord.Embed(title="Error", description=f"Could not find leetcode account with the username {username}", color=discord.Color.red())
+            await ctx.send(embed=error_embed)
+    except commands.errors.CommandError as ce:
+        print(f"CommandError: {ce}")
+        error_embed = discord.Embed(title="Command Error", description="An error occurred during command execution.", color=discord.Color.red())
+        await ctx.send(embed=error_embed)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        error_embed = discord.Embed(title="Unexpected Error", description="An unexpected error occurred.", color=discord.Color.red())
+        await ctx.send(embed=error_embed)
 
-#     await bot.process_commands(message) # when overriding discords built in methods
+@bot.command()
+async def comparestats(ctx):
+    if ctx.channel.name != "testing":
+        return
+    try:
+        # first username
+        embed = discord.Embed(title="", color=discord.Color.yellow())
+        embed.title = "Enter the first username"
+        await ctx.send(embed=embed)
+        # wait for user input
+        username1 = await bot.wait_for("message", check=lambda m: m.author == ctx.author, timeout=60.0)
+        username1 = username1.content
+
+        # second username
+        embed.title = "Enter the second username"
+        await ctx.send(embed=embed)
+        # wait for user input
+        username2 = await bot.wait_for("message", check=lambda m: m.author == ctx.author, timeout=60.0)
+        username2 = username2.content
+        
+        df_comparison = await compareUsers(username1, username2)
     
+        if df_comparison is not None:
+            comparison_message = df_comparison.to_string(index=False)
+            
+            await ctx.send(f"```{comparison_message}```")
+        else:
+            # Error occurred during comparison
+            error_embed = discord.Embed(title="Error", description="Could not find leetcode account with the username", color=discord.Color.red())
+            await ctx.send(embed=error_embed)
+    except commands.errors.CommandError as ce:
+        print(f"CommandError: {ce}")
+        error_embed = discord.Embed(title="Command Error", description="An error occurred during command execution.", color=discord.Color.red())
+        await ctx.send(embed=error_embed)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        error_embed = discord.Embed(title="Unexpected Error", description="An unexpected error occurred.", color=discord.Color.red())
+        await ctx.send(embed=error_embed)
+
+# Handle wrong command
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CommandNotFound):
+        error_embed = discord.Embed(title="Command Not Found", description="The command you entered does not exist.", color=discord.Color.red())
+        await ctx.send(embed=error_embed)
+
+# lists all available commands (maybe help does same?)
+@bot.command()
+async def listcommands(ctx):
+    command_list = [command.name for command in bot.commands]
+    command_list.sort()
+    commands_str = "\n".join(command_list)
+    
+    embed = discord.Embed(title="Available Commands", description=f"```\n{commands_str}\n```", color=discord.Color.green())
+    await ctx.send(embed=embed)
+
 TOKEN = getenv("TOKEN")
 bot.run(TOKEN)
--- service/leetStats.py
+++ service/leetStats.py
@@ -5,24 +5,70 @@ import json
 async def getLeetStats(username):
     url = f"https://leetcode-stats-api.herokuapp.com/{username}"
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.text()
-            json_data = json.loads(data)
-            return convert_to_dataframe(json_data=json_data)
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    data = await response.text()
+                    json_data = json.loads(data)
+                    return convert_to_dataframe(json_data=json_data)
+                else:
+                    print(f"Error: {response.status} - {response.reason}")
+                    return None  # or raise an exception if appropriate
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None  # or raise an exception if appropriate
             
 
 def convert_to_dataframe(json_data):
-    df = pd.DataFrame({
-        "Category": ["Total Solved", "Total Questions", "Easy Solved", "Total Easy",
-                     "Medium Solved", "Total Medium", "Hard Solved", "Total Hard",
-                     "Acceptance Rate", "Ranking", "Contribution Points", "Reputation"],
-        "Value": [json_data["totalSolved"], json_data["totalQuestions"],
-                  json_data["easySolved"], json_data["totalEasy"],
-                  json_data["mediumSolved"], json_data["totalMedium"],
-                  json_data["hardSolved"], json_data["totalHard"],
-                  json_data["acceptanceRate"], json_data["ranking"],
-                  json_data["contributionPoints"], json_data["reputation"]]
+    try:
+        df = pd.DataFrame({
+            "Category": ["Total Solved", "Easy Solved", "Medium Solved", "Hard Solved",
+                         "Acceptance Rate", "Ranking", "Contribution Points", "Reputation"],
+            "Value": [json_data["totalSolved"], json_data["easySolved"],
+                      json_data["mediumSolved"], json_data["hardSolved"],
+                      json_data["acceptanceRate"], json_data["ranking"],
+                      json_data["contributionPoints"], json_data["reputation"]]
+        })
+
+        # some cases zero only was returned
+        if df["Value"].sum() == 0:
+            return None 
+        
+        return df
+    except KeyError as ke:
+        print(f"KeyError: {ke} - Check if the expected keys are present in the JSON data.")
+        return None
+    except Exception as e:
+        print(f"An error occurred during DataFrame creation: {e}")
+        return None  
+
+async def compareUsers(username1, username2):
+    df_user1 = await getLeetStats(username1)
+    df_user2 = await getLeetStats(username2)
+
+    if df_user1 is None or df_user2 is None:
+        # add proper handling later
+        return None
+
+    # new df for comparison
+    df_comparison = pd.DataFrame({
+        "Category": df_user1["Category"],
+        f"{username1}": df_user1["Value"],
+        f"{username2}": df_user2["Value"]
     })
 
-    return df
+    # Winner column based on the comparison
+    df_comparison["Winner"] = df_comparison.apply(lambda row: determine_winner(row, username1, username2), axis=1)
+
+    return df_comparison
+
+def determine_winner(row, username1, username2):
+    category = row["Category"]
+    value1 = row[username1]
+    value2 = row[username2]
+
+    if category == "Ranking":
+        return username1 if value1 < value2 else username2
+    else:
+        return username1 if value1 > value2 else username2
